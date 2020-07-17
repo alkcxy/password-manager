a9d48502d55213da0cd7343fac67655e2b30fffb
Alessio Caradossi <acaradossi@hotels.com>
2020-07-17 08:45:07 +0200

    added bootstrap

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ bundle install
 # bin/rails g scaffold Credential name:string user:string password:PasswordType url:string note:text
 # bin/rails g scaffold User name:string email:string password:string
 rails g controller sessions new create login welcome 
+# yarn add bootstrap jquery popper.js
 rails s -p 3000 -b '0.0.0.0'
