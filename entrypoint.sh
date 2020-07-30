83d18c5f976844f1e39dc5be6bc25c7ef0960f49
Alessio Caradossi <acaradossi@hotels.com>
2020-07-30 15:46:44 +0200

    some improvements to build a 32bit arm docker image that uses a mongodb 64bit arm image

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,8 @@ rm -f tmp/pids/server.pid
 # bundle update
 # rails g mongoid:config
 # rails webpacker:install
-# yarn install
-# yarn upgrade
-# bin/rails g scaffold Credential name:string user:string password:PasswordType url:string note:text
-# bin/rails g scaffold User name:string email:string password:string
-# rails g controller sessions new create login welcome 
-# yarn add bootstrap jquery popper.js
 rake db:mongoid:create_indexes
+yarn upgrade
+yarn install --check-files
+RAILS_ENV=production rails assets:precompile
 rails s -p 3000 -b '0.0.0.0'
